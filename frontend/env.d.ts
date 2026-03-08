32c3527376848a9a720c7c3f1499514bf4e1692a
Kirill Shilov <tvix2000@yandex.ru>
2026-03-09 01:11:24 +0400

    feat/task-10 - updated project to vite and vitest (#10)
    Co-authored-by: kashilov

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite/client" />
-/// <reference types="unplugin-vue-router/client" /> //
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
